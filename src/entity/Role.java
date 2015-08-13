0f50591e907f1cd9a9907d92f2dc8514786b14f7
flakedzz <ninganyi@live.com>
2015-08-13 22:30:33 +0800

    Signed-off-by: flakedzz <ninganyi@live.com>

---
+++ src/entity/Role.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.util.Date;
+
+public class Role {
+	private Integer role_id;
+	private String role_name;
+	private Integer flag;
+	private Date date;
+	private Date sys_date;
+	public Integer getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(Integer role_id) {
+		this.role_id = role_id;
+	}
+	public String getRole_name() {
+		return role_name;
+	}
+	public void setRole_name(String role_name) {
+		this.role_name = role_name;
+	}
+	public Integer getFlag() {
+		return flag;
+	}
+	public void setFlag(Integer flag) {
+		this.flag = flag;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getSys_date() {
+		return sys_date;
+	}
+	public void setSys_date(Date sys_date) {
+		this.sys_date = sys_date;
+	}
+	
+
+}
