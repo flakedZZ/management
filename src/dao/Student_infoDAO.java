fa1eb8ca5793dffcf4f15c2051368ddbfc0e8dde
guofangjun <1805648841@qq.com>
2015-08-17 12:48:20 +0800

    Signed-off-by: guofangjun <1805648841@qq.com>

---
+++ src/dao/Student_infoDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+import entity.Student_info;
+
+
+
+public interface Student_infoDAO {
+	void save(Student_info stu);
+	Student_info getStudentInfo(int student_number,String student_name);//这个方法在学生信息类里并没有用
+	Student_info getStudentInfo(int student_number);
+	void delStudentInfo(int id);
+	void update(Student_info stu);
+	Student_info findById(int id);
+	List<Student_info> findAll();
+}
---
+++ src/entity/User_role.java
@@ -0,0 +1,50 @@
+package entity;
+
+import java.util.Date;
+
+public class User_role {
+	private Integer user_role_id;
+	private Integer user_id;
+	private Integer role_id;
+	private  Integer flag;
+	private Date date;
+	private Date sys_date;
+	public Integer getUser_role_id() {
+		return user_role_id;
+	}
+	public void setUser_role_id(Integer user_role_id) {
+		this.user_role_id = user_role_id;
+	}
+	public Integer getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	public Integer getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(Integer role_id) {
+		this.role_id = role_id;
+	}
+	public Integer getFlag() {
+		return flag;
+	}
+	public void setFlag(Integer flag) {
+		this.flag = flag;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Date getSys_date() {
+		return sys_date;
+	}
+	public void setSys_date(Date sys_date) {
+		this.sys_date = sys_date;
+	}
+	
+	
+}
